6425f6d86ebed3057eb632ba6fa2fb653aa882c6
Максим Пенкин <m.penkin@gmail.com>
2020-10-28 09:26:24 +1000

    successCase modified

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -26,7 +26,7 @@ public class CardDeliveryTest {
     @Test
     void successCase() {
         open("http://localhost:9999/");
-        $("[data-test-id=city] input[class=input__control]").setValue(faker.address().city());
+        $("[data-test-id=city] input[class=input__control]").setValue(faker.address().city()); // Изменились требовнаия  к городам (Новокузнецк, Сочи, Тольяти)
         $("[data-test-id=date] [class='input__box'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE); // очистка инпута даты;
         LocalDate meetingDate = LocalDate.now().plusDays(7);  // вычисление текущей даты + 7 дней;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy"); // перевод даты в нужный формат;
@@ -34,11 +34,15 @@ public class CardDeliveryTest {
         $("[data-test-id=name] [type=text]").setValue(faker.name().fullName());
         FakeValuesService fakeValuesService = new FakeValuesService(
                 new Locale("ru"), new RandomService());
-        $("[data-test-id=phone] [type=tel]").setValue(fakeValuesService.numerify("###########"));
+        $("[data-test-id=phone] [type=tel]").setValue(fakeValuesService.numerify("7##########"));
         $("[data-test-id=agreement]").click();
         $("[type=button] [class='button__text']").click();
-        $("[data-test-id=notification]").waitUntil(Condition.visible, 15000);
-        String successText = $("[data-test-id=notification]").getText(); // получить текст и дату из нотификейшена;
-        assertEquals("Успешно!\nВстреча успешно забронирована на " + meetingDate.format(formatter), successText); // сравнить текст и дату из нотификейшена с ожидаемым текстом и текущей датой;
+        $("[data-test-id=success-notification]").waitUntil(Condition.visible, 3000);
+        $("[data-test-id=date] [class='input__box'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE); // очистка инпута даты;
+        LocalDate meetingDate2 = LocalDate.now().plusDays(9);  // вычисление текущей даты + 7 дней;
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd.MM.yyyy"); // перевод даты в нужный формат;
+        $("[data-test-id=date] [type=tel]").setValue(meetingDate2.format(formatter2)); // ввод даты в нужном формате в инпут;
+        String successText = $("[data-test-id=success-notification]").getText(); // получить текст и дату из нотификейшена;
+        assertEquals("Успешно!\nВстреча успешно запланирована на " + meetingDate2.format(formatter2), successText); // сравнить текст и дату из нотификейшена с ожидаемым текстом и текущей датой;
     }
 }
