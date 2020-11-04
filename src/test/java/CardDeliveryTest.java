5dc72c81a46b2ed2d0ba50f73a0c60501b08ad02
camenzindmp <camenzindmp@gmail.com>
2020-11-04 16:11:44 +1000

    UserGenerator.java refactor

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -10,16 +10,17 @@ public class CardDeliveryTest {
     @Test
     void successCase() {
         open("http://localhost:9999/");
-        $("[data-test-id=city] input[class=input__control]").setValue((String) DataGenerator.CardDeliveryForm.CitiesArray); // берем рандомный город из массива CitiesArray;
+        UserGenerator.ClientInfo = UserGenerator.getClientInfo();
+        $("[data-test-id=city] input[class=input__control]").setValue((String) UserGenerator.CardDeliveryForm.CitiesArray); // берем рандомный город из массива CitiesArray;
         $("[data-test-id=date] [class='input__box'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE); // очистка инпута даты;
-        $("[data-test-id=date] [type=tel]").setValue(DateGenerator.generateDate); // ввод даты в нужном формате в инпут;
-        $("[data-test-id=name] [type=text]").setValue(DataGenerator.ClientInfo.generateByClient.fullName); // ввод сгенерированного имени;
-        $("[data-test-id=phone] [type=tel]").setValue(DataGenerator.ClientInfo.phoneNumber); // ввод сгенерированного номера телефона;
+        $("[data-test-id=date] [type=tel]").setValue(UserGenerator.generateDate); // ввод даты в нужном формате в инпут;
+        $("[data-test-id=name] [type=text]").setValue(UserGenerator.ClientInfo.generateByClient.fullName); // ввод сгенерированного имени;
+        $("[data-test-id=phone] [type=tel]").setValue(UserGenerator.ClientInfo.phoneNumber); // ввод сгенерированного номера телефона;
         $("[data-test-id=agreement]").click(); // клик по чб;
         $("[type=button] [class='button__text']").click(); // клик по копке "Отправить";
         $("[data-test-id=success-notification]").shouldBe(Condition.visible); // Проверка нтификейшена (ДОРАБОТАТЬ: сделать проверку текста и даты);
         $("[data-test-id=date] [class='input__box'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE); // очистка инпута даты;
-        $("[data-test-id=date] [type=tel]").setValue(DataGenerator.DateGenerator.newMeetingDate.newFormatter); // ввод даты в нужном формате в инпут;
+        $("[data-test-id=date] [type=tel]").setValue(UserGenerator.UserGenerator.newMeetingDate.newFormatter); // ввод даты в нужном формате в инпут;
         String successText = $("[data-test-id=success-notification]").getText(); // получить текст и дату из нотификейшена;
         assertEquals("Успешно!\nВстреча успешно запланирована на " + newMeetingDate.format(newFormatter), successText); // сравнить текст и дату из нотификейшена с ожидаемым текстом и текущей датой;
     }
---
+++ src/test/java/UserGenerator.java
@@ -0,0 +1,49 @@
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+
+public class UserGenerator {
+    private UserGenerator() {
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class ClientInfo {
+        private String fullName;
+        private String phoneNumber;
+        private String city;
+        private String meetingDate;
+        private String newMeetingDate;
+    }
+
+    public static ClientInfo getClientInfo() {
+        Faker faker = new Faker(new Locale("ru"));
+        return new ClientInfo(
+                faker.name().fullName(),
+                faker.phoneNumber().phoneNumber()
+        );
+    }
+
+    public static ClientInfo generateCity() {
+        private String[] cities = new String[]{"Москва", "Казань", "Владивосток", "Екатеринбург", "Воронеж"};
+        private Random rand = new Random();
+        public String getRandomCity () {
+            return cities[rand.nextInt(4)];
+        }
+    }
+
+    public static ClientInfo generateDate() {
+        LocalDate meetingDate = LocalDate.now().plusDays(7);  // вычисление текущей даты + 7 дней;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate newMeetingDate = LocalDate.now().plusDays(9);  // вычисление текущей даты + 9 дней;
+        DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return new ClientInfo(meetingDate);
+    }
+}
+
