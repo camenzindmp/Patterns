0c8fdf6f6bb186a943be2e113776d6663b00cc3f
camenzindmp <camenzindmp@gmail.com>
2020-11-07 16:04:59 +1000

    UserGenerator.java refactoring

--- src/test/java/ru/netology/data/UserGenerator.java
+++ src/test/java/ru/netology/data/UserGenerator.java
@@ -21,31 +21,42 @@ public class UserGenerator {
         private String phoneNumber;
         private String city;
         private String meetingDate;
+        private String formatter;
         private String newMeetingDate;
+        private String newFormatter;
     }
 
     public static ClientInfo getClientInfo() {
         Faker faker = new Faker(new Locale("ru"));
         return new ClientInfo(
                 faker.name().fullName(),
-                faker.phoneNumber().phoneNumber()
-        );
+                faker.phoneNumber().phoneNumber(),
+                generateCity().city,
+                generateMeetingDate().meetingDate,
+                generateMeetingDate().formatter,
+                generateNewMeetingDate().newMeetingDate,
+                generateNewMeetingDate().newFormatter);
     }
 
     public static ClientInfo generateCity() {
-        private String[] cities = new String[]{"Москва", "Казань", "Владивосток", "Екатеринбург", "Воронеж"};
-        private Random rand = new Random();
-        public String getRandomCity () {
-            return cities[rand.nextInt(4)];
+        String[] cities = new String[] {"Москва", "Казань", "Владивосток", "Екатеринбург", "Воронеж"};
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(cities.length);
+        ClientInfo.city = cities[randomIndex];
+        return new ClientInfo().city;
         }
-    }
 
-    public static ClientInfo generateDate() {
+    public static ClientInfo generateMeetingDate() {
         LocalDate meetingDate = LocalDate.now().plusDays(7);  // вычисление текущей даты + 7 дней;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return new ClientInfo().formatter;
+    }
+
+    public static ClientInfo generateNewMeetingDate() {
         LocalDate newMeetingDate = LocalDate.now().plusDays(9);  // вычисление текущей даты + 9 дней;
         DateTimeFormatter newFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        return new ClientInfo(meetingDate);
+        return new ClientInfo().newFormatter;
     }
+
 }
 
--- src/test/java/ru/netology/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -1,9 +1,12 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import ru.netology.data.UserGenerator;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,18 +15,19 @@ public class CardDeliveryTest {
     @Test
     void successCase() {
         open("http://localhost:9999/");
-        UserGenerator.ClientInfo = UserGenerator.getClientInfo();
-        $("[data-test-id=city] input[class=input__control]").setValue((String) UserGenerator.CardDeliveryForm.CitiesArray); // берем рандомный город из массива CitiesArray;
+        val ClientInfo = UserGenerator.getClientInfo();
+        $("[data-test-id=city] input[class=input__control]").setValue(UserGenerator.getClientInfo().getCity()); // берем рандомный город из массива CitiesArray;
         $("[data-test-id=date] [class='input__box'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE); // очистка инпута даты;
-        $("[data-test-id=date] [type=tel]").setValue(UserGenerator.generateDate); // ввод даты в нужном формате в инпут;
-        $("[data-test-id=name] [type=text]").setValue(UserGenerator.ClientInfo.generateByClient.fullName); // ввод сгенерированного имени;
-        $("[data-test-id=phone] [type=tel]").setValue(UserGenerator.ClientInfo.phoneNumber); // ввод сгенерированного номера телефона;
+        $("[data-test-id=date] [type=tel]").setValue(UserGenerator.getClientInfo().getMeetingDate()); // ввод даты в нужном формате в инпут;
+        $("[data-test-id=name] [type=text]").setValue(UserGenerator.getClientInfo().getFullName()); // ввод сгенерированного имени;
+        $("[data-test-id=phone] [type=tel]").setValue(UserGenerator.getClientInfo().getPhoneNumber()); // ввод сгенерированного номера телефона;
         $("[data-test-id=agreement]").click(); // клик по чб;
         $("[type=button] [class='button__text']").click(); // клик по копке "Отправить";
-        $("[data-test-id=success-notification]").shouldBe(Condition.visible); // Проверка нтификейшена (ДОРАБОТАТЬ: сделать проверку текста и даты);
+//        $("[data-test-id=success-notification]").shouldBe(Condition.visible); // Проверка нтификейшена (ДОРАБОТАТЬ: сделать проверку текста и даты);
+        $(byText("Успешно!\nВстреча успешно запланирована на " + UserGenerator.getClientInfo().getMeetingDate().format(UserGenerator.getClientInfo().getFormatter()))).shouldBe(Condition.visible);
         $("[data-test-id=date] [class='input__box'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE); // очистка инпута даты;
-        $("[data-test-id=date] [type=tel]").setValue(UserGenerator.UserGenerator.newMeetingDate.newFormatter); // ввод даты в нужном формате в инпут;
+        $("[data-test-id=date] [type=tel]").setValue(UserGenerator.getClientInfo().getNewMeetingDate()); // ввод даты в нужном формате в инпут;
         String successText = $("[data-test-id=success-notification]").getText(); // получить текст и дату из нотификейшена;
-        assertEquals("Успешно!\nВстреча успешно запланирована на " + newMeetingDate.format(newFormatter), successText); // сравнить текст и дату из нотификейшена с ожидаемым текстом и текущей датой;
+        assertEquals("Успешно!\nВстреча успешно запланирована на " + UserGenerator.getClientInfo().getNewMeetingDate().format(UserGenerator.getClientInfo().getNewFormatter()), successText); // сравнить текст и дату из нотификейшена с ожидаемым текстом и текущей датой;
     }
 }
